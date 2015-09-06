43314160986dadece6d50ea0720f47e6d60dd81b
Lei Ming(雷明) <poetcoder@outlook.com>
2015-09-06 09:48:04 +0800

    support es6

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const Arthas = {
-  Checkbox: require('./src/js/components/Checkbox/Checkbox.jsx'),
-  Dropdown: require('./src/js/components/Dropdown/Dropdown.jsx'),
-  Tab: require('./src/js/components/Tab/Tab.jsx')
-};
-
-module.exports = Arthas;
-
-
+module.exports = require('./src/');
