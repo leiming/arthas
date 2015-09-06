975ced5f29f955ff3c7a6735b70f03fb0ed94c17
Lei Ming(雷明) <poetcoder@outlook.com>
2015-09-06 09:34:50 +0800

    fix index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./src/');
+const Arthas = {
+  Checkbox: require('./src/js/components/Checkbox/Checkbox.jsx'),
+  Dropdown: require('./src/js/components/Dropdown/Dropdown.jsx'),
+  Tab: require('./src/js/components/Tab/Tab.jsx')
+};
+
+module.exports = Arthas;
+
+
