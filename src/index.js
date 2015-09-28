ddc7de1cca521ade9bea475f7eaa9cd0d14cf065
Oscar360 <rxl719704258@gmail.com>
2015-09-28 16:27:51 +0800

    rename dropdown-iframe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var Arthas = {
   'Checkbox': require('./js/components/Checkbox/Checkbox'),
   'Dropdown': require('./js/components/Dropdown/Dropdown'),
-  'Dropdown-Iframe': require('./js/components/Dropdown/Dropdown-Iframe'),
+  'DropdownIframe': require('./js/components/Dropdown/DropdownIframe'),
   'Tab': require('./js/components/Tab/Tab')
 };
 
